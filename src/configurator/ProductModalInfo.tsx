2e3196b6c5ee35058c4c6f3b16f573d890d461d4
Kevin Oh <cmyk.k4@gmail.com>
2021-04-26 00:19:24 -0400

    Start updating the format of the modal content

--- src/configurator/ProductModalInfo.tsx
+++ src/configurator/ProductModalInfo.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEventHandler, useState } from "react"
+import React, { useState } from "react"
 import { Keyboard } from "types/keyboard"
 import styles from "./ProductModalInfo.module.scss"
 
@@ -7,15 +7,18 @@ interface Props {
 }
 
 const ProductModalInfo: React.FC<Props> = ({ product }: Props) => {
-  const { brand, size, full_title, product_name, interfaces, price } = product
+  const { brand, size, frame_color, full_title, product_name, interfaces, price } = product
 
   return (
     <div className={styles.ProductModalInfo}>
       <div className={styles.productImageWrapper}></div>
       <div className={styles.productDetails}>
-        <span className={styles.productBrand}>{brand}</span>
-        <span className={styles.productTitle}>{product_name}</span>
+        <span className={styles.productBrand}>{brand} // </span>
+        <span className={styles.productTitle}>
+          {product_name} ({frame_color})
+        </span>
         <div className={styles.productPrice}>{price}</div>
+        <div className={styles.productDescription}>{full_title}</div>
       </div>
     </div>
   )
--- templates/page-content.tsx
+++ templates/page-content.tsx
@@ -11,7 +11,7 @@ const PageContent = ({ title, subtitle, style, children }: PageProps) => {
   return (
     <div className={styles.content}>
       <div style={{ marginBottom: "1.5rem" }}>
-        <h1>{title} </h1>
+        <h1 className={styles.pageHeading}>{title} </h1>
         <hr className={styles.headerDivider} />
         <span>{subtitle}</span>
       </div>
