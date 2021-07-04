f38ca89876da5f5ecf0d9affc94092a67ab94c10
Kevin Oh <cmyk.k4@gmail.com>
2021-07-04 01:16:09 -0400

    comment

--- pages/configurator.tsx
+++ pages/configurator.tsx
@@ -34,7 +34,6 @@ export default function Configurator() {
         allProducts,
       )
 
-      // setProductsFilteredByMultipleSelect(productsFilteredByMultipleSelect)
       setProducts(products)
     }
 
